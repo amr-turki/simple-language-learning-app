2f45d89d71049d666bcf983e7ca8d9afccc7242c
Amr Turki <amrturki79@gmail.com>
2026-03-06 14:47:50 +0200

    Finish Family Members Screen

--- lib/view/color_view.dart
+++ lib/view/color_view.dart
@@ -1,12 +1,12 @@
-
-
 import 'package:flutter/material.dart';
+import 'package:quick_speak/model/item_model.dart';
+import 'package:quick_speak/widget/item.dart';
 
 class ColorView extends StatelessWidget {
   const ColorView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold();
   }
 }
--- lib/view/family_view.dart
+++ lib/view/family_view.dart
@@ -1,10 +1,74 @@
 import 'package:flutter/material.dart';
+import 'package:quick_speak/model/item_model.dart';
+import 'package:quick_speak/widget/item.dart';
 
 class FamilyView extends StatelessWidget {
   const FamilyView({super.key});
 
+  final List<ItemModel> familyMembersList = const [
+    ItemModel(
+      englishMeaning: 'Father',
+      italianMeaning: 'Padre',
+      color: Color(0xFF55EFC4),
+      image: 'assets/family_members/father.png',
+      sound: 'family_members/sounds/father.mp3',
+    ),
+    ItemModel(
+      englishMeaning: 'Mother',
+      italianMeaning: 'Madre',
+      color: Color(0xFF55EFC4),
+      image: 'assets/family_members/mother.png',
+      sound: 'family_members/sounds/mother.mp3',
+    ),
+    ItemModel(
+      englishMeaning: 'Son',
+      italianMeaning: 'Figlio',
+      color: Color(0xFF55EFC4),
+      image: 'assets/family_members/son.png',
+      sound: 'family_members/sounds/son.mp3',
+    ),
+    ItemModel(
+      englishMeaning: 'Daughter',
+      italianMeaning: 'Figlia',
+      color: Color(0xFF55EFC4),
+      image: 'assets/family_members/daughter.png',
+      sound: 'family_members/sounds/daughter.mp3',
+    ),
+    ItemModel(
+      englishMeaning: 'Grandfather',
+      italianMeaning: 'Nonno',
+      color: Color(0xFF55EFC4),
+      image: 'assets/family_members/grandfather.png',
+      sound: 'family_members/sounds/grandfather.mp3',
+    ),
+    ItemModel(
+      englishMeaning: 'Grandmother',
+      italianMeaning: 'Nonna',
+      color: Color(0xFF55EFC4),
+      image: 'assets/family_members/grandmother.png',
+      sound: 'family_members/sounds/grandmother.mp3',
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xFFFFF6E3),
+        title: Text(
+          'Family Members',
+          style: TextStyle(
+            fontSize: 24,
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      body: ListView.builder(
+        itemCount: familyMembersList.length,
+        itemBuilder: (context, index) {
+          return Item(model: familyMembersList[index]);
+        },
+      ),
+    );
   }
 }
